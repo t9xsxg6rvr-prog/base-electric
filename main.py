a34252c96838381a4541dcd8f30c9f2c93569bad
t9xsxg6rvr-prog <t9xsxg6rvr@privaterelay.appleid.com>
2026-05-08 08:55:14 -0400

    Ajout du timer fonctionnel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtCore import Qt, QRectF
+from PyQt6.QtCore import Qt, QRectF, QTimer
 from PyQt6.QtGui import QBrush, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGraphicsView, QGraphicsScene, QVBoxLayout
 
@@ -24,6 +24,17 @@ class FenetrePrincipale(QMainWindow):
         self.canvas = Canvas(self)
         self.layout_central.addWidget(self.canvas)
 
+        # Création du Timer 
+        self.main_timer = QTimer()
+        self.count = 0
+        self.main_timer.timeout.connect(self.main_update_loop)
+        self.main_timer.start(100) # 10 fois chaque seconde
+
+    def main_update_loop(self):
+        self.count += 1 
+        if self.count % 100 == 0:
+            print("Nombre de secondes de fonctionnement: " + str(self.count / 10))
+
 class Canvas(QGraphicsView):
     def __init__(self, fenetre_principale):
         super().__init__()
@@ -31,8 +42,6 @@ class Canvas(QGraphicsView):
         self.scene_graphique = QGraphicsScene(QRectF(0, 0, 2000, 2500))
         self.setScene(self.scene_graphique)
         self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
 
     def get_fenetre_principale(self):
         return self.__fenetre_principale
