4143f972d9bceee4c058f9eddfdd734ffe76548a
t9xsxg6rvr-prog <t9xsxg6rvr@privaterelay.appleid.com>
2026-05-08 09:23:52 -0400

    Ajout d'une résistance graphique de base

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import sys
 from PyQt6.QtCore import Qt, QRectF, QTimer
 from PyQt6.QtGui import QBrush, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGraphicsView, QGraphicsScene, QVBoxLayout
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QWidget, 
+    QGraphicsView, 
+    QGraphicsScene, 
+    QVBoxLayout, 
+    QHBoxLayout, 
+    QPushButton, 
+    QGraphicsItemGroup, 
+    QGraphicsRectItem, 
+    QGraphicsEllipseItem,
+)
 
 def main():
     app = QApplication(sys.argv)
@@ -18,11 +30,14 @@ class FenetrePrincipale(QMainWindow):
         #Création widget central & layout
         self.widget_central = QWidget()
         self.setCentralWidget(self.widget_central)
-        self.layout_central = QVBoxLayout(self.widget_central)
+        self.layout_central = QHBoxLayout(self.widget_central)
 
         # Ajouter le Canvas
         self.canvas = Canvas(self)
         self.layout_central.addWidget(self.canvas)
+
+        # Ajouter la Barre de Composantes
+        self.barre_droite = BarreComposante(self.layout_central, self.canvas.scene_graphique)
 
         # Création du Timer 
         self.main_timer = QTimer()
@@ -39,7 +54,7 @@ class Canvas(QGraphicsView):
     def __init__(self, fenetre_principale):
         super().__init__()
         self.__fenetre_principale = fenetre_principale
-        self.scene_graphique = QGraphicsScene(QRectF(0, 0, 2000, 2500))
+        self.scene_graphique = QGraphicsScene(QRectF(-1000, -1000, 2000, 2000))
         self.setScene(self.scene_graphique)
         self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
 
@@ -53,6 +68,42 @@ class Canvas(QGraphicsView):
     def desactiver_mouvement_canvas(self):
         self.setDragMode(QGraphicsView.DragMode.NoDrag)
         return "Mouvement du canvas désactivé"
+    
+class BarreDroite(QWidget):
+    def __init__(self, layout_parent, scene):
+        super().__init__()
+        self.scene = scene
+        self.layout_barre_droite = QVBoxLayout(self)
+        layout_parent.addWidget(self)
+
+
+class BarreComposante(BarreDroite):
+    def __init__(self, layout_parent, scene):
+        super().__init__(layout_parent, scene)
+        bouton_source = QPushButton("Source")
+        self.layout_barre_droite.addWidget(bouton_source)
+        bouton_resistance = QPushButton("Résistance")
+        self.layout_barre_droite.addWidget(bouton_resistance)
+        bouton_resistance.clicked.connect(self.ajout_source)
+
+    # Fonction d'ajout 
+    def ajout_source(self):
+        resistance = Resistance()
+        self.scene.addItem(resistance)
+
+
+class Composante(QGraphicsItemGroup):
+    def __init__(self):
+        super().__init__()
+        self.setPos(0, 0)
+
+
+class Resistance(Composante):
+    def __init__(self):
+        super().__init__()
+        self.rectangle = QGraphicsRectItem(QRectF(0, 0, 100, 100))
+        self.rectangle.setBrush(QColor(255, 0, 0))
+        self.addToGroup(self.rectangle)
         
 
 if __name__ == "__main__":
